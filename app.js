cf03841a23403e812cc26a8dafa319d518659a2b
Phat Dang <phatdang2909@gmail.com>
2025-07-06 12:57:23 +0700

    Commit

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-const PORT = 3000; // Static port, no dotenv needed
+const PORT = 3000;
 
 app.use(express.json());
 
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Mount routes
 app.use('/', routes);
-app.use('/health', routes);
-app.use('/users', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
